27ff8ab658ee850f6aea00446966f7c676229d11
guerlak <guerlak@hotmail.com>
2021-05-01 19:27:47 -0300

    fix fonts3

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,13 +36,13 @@ export default class MyDocument extends Document {
                 <Head>
                     <meta charSet='utf-8' />
                     <link rel="icon" href="/favicon.ico" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com">
-                        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
                 </Head>
-                    <body>
-                        <Main />
-                        <NextScript />
-                    </body>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
             </Html>
         )
     }
